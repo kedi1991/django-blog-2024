1c23c9e84d379e740fd1af86b6a837224ff2abf5
kedi1991 <okurut.henry@yahoo.com>
2024-01-02 02:24:30 +0000

    add the comments model

--- blog/models.py
+++ blog/models.py
@@ -10,3 +10,14 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='commenter')
+    body = models.TextField(blank=False)
+    approved = models.BooleanField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    
